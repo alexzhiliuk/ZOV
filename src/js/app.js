3a7871865a64e05c7a458ad623c2e3b08f93732a
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-10-22 18:44:12 +0300

    hints

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 import * as markers from './modules/markers.js';
 import * as popup from './modules/popup.js';
 import * as mainPadding from './modules/mainPadding.js';
+import * as hints from './modules/hints.js';
 
 // flsFunctions.isWebp()
 
---
+++ src/js/modules/hints.js
@@ -0,0 +1,35 @@
+function moveHint(el) {
+    let objectId = $(el).attr("data-hint-object"),
+    hintObject = $(`#${objectId}`),
+    top = hintObject.offset().top - 50 + hintObject.outerHeight() / 2,
+    left = hintObject.offset().left + hintObject.outerWidth() + 20
+
+    console.log(hintObject.offset())
+
+    $(el)
+        .css("position", "absolute")
+        .css("top", top)
+        .css("left", left)
+        .css("width", "435px")
+
+}
+    
+
+
+$(".hint[data-hint-object]").each(function(i, el) {
+    moveHint(el)
+})
+
+$(".hint__close").click(function() {
+    $(this).parents(".hint").hide()
+})
+
+$(window).resize(function() {
+    if (window.innerWidth < 1024) {
+        $(".hint[data-hint-object]")
+            .css("position", "")
+            .css("top", "")
+            .css("left", "")
+            .css("width", "")
+    }
+})
