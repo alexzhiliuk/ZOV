69209ceb123f08ac144f726bf13e10d44d7d68c8
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-10-28 16:57:37 +0300

    fix

--- src/js/modules/mainPadding.js
+++ src/js/modules/mainPadding.js
@@ -1,14 +1,14 @@
 function setMainTopPadding() {
     let mainPadding, bannerPadding
     if ($(window).innerWidth() > 1024) {
-        mainPadding = $(".header").outerHeight() + 20 + $(".header__row_out").outerHeight() + 28;
+        mainPadding = $(".header").outerHeight() + 20 + $(".header__row_out").outerHeight() + 45;
     } else {
         mainPadding = $(".header").outerHeight() + 35
         bannerPadding = $(".header").outerHeight() + 35
     }
 
     $("main").css("padding-top", `${mainPadding}px`);
-    bannerPadding ? $(".main-banner").css("padding-top", `${bannerPadding}px`).css("margin-top", `-${bannerPadding}px`) : $(".main-banner").css("padding-top", '')
+    bannerPadding ? $(".main-banner").css("padding-top", `${bannerPadding}px`).css("margin-top", `-${bannerPadding}px`) : $(".main-banner").css("padding-top", '').css("margin-top", ``);
 }
 setMainTopPadding()
 
